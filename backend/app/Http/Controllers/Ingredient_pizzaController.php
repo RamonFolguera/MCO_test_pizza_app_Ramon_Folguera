c1608de9b9291ef5c1c35d25b2feade5c128e4c9
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-05-10 20:07:11 +0200

    feat: created endpoint to delete ingredient en ingredient_pizza table. added routes.

--- backend/app/Http/Controllers/Ingredient_pizzaController.php
+++ backend/app/Http/Controllers/Ingredient_pizzaController.php
@@ -45,4 +45,34 @@ class Ingredient_pizzaController extends Controller
         }
     }
 
+    public function removeIngredientToPizzaByPizzaId(Request $request, $id)
+    {
+        try{
+
+            Log::info("Adding ingredients working");
+            $pizza = Pizza::find($id);
+            $pizzaId = $pizza->id;
+            $ingredientId = $request->input('ingredient_id');
+            
+            Ingredient_pizza::where('pizza_id',"=", $pizzaId)->where('ingredient_id', '=', $ingredientId)->delete();
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Ingredient successfully deleted",
+                ],
+                200
+            );
+
+        }catch (\Throwable $th) {
+            Log::error("DELETING INGREDIENT: " . $th->getMessage());
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error deleting ingredients"
+                ],
+                500
+            );
+        }
+    }
 }
