554b229095d4a35c11c602caed709b5d6f9188ee
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-05-10 16:30:42 +0200

    feat: created frontend React file, installed react-router-dom, added home layout. in main.jsx added browserRouter y StrictMode

---
+++ frontend/src/layout/Body/body.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Home } from '../00-Home/Home'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+export const Body = () => {
+  return (
+    <> 
+    <Routes>
+        <Route path="*" element={<Navigate to="/"/>}/>
+        <Route path="/" element={<Home/>}/>
+    
+    </Routes>
+    </>
+  )
+}
