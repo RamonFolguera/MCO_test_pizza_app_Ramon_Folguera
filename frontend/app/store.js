87f2629dcef75087484c23e9bb1056bd34429f68
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-05-11 13:18:05 +0200

    feat: created detailsSlice to save selected Pizza details in redux

--- frontend/app/store.js
+++ frontend/app/store.js
@@ -5,12 +5,10 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 
 import thunk from 'redux-thunk';
-import userSlice from '../layout/userSlice';
-import artworkSlice from '../layout/artworkSlice';
 import detailsSlice from '../layout/detailsSlice';
 
 const reducers = combineReducers({
-   
+    details: detailsSlice,
   })
 
 const persistConfig = {
